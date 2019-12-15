425b07c9166e899482182acb7dbd8d3a17c73e3f
Jiangdedong <1308242265@qq.com>
2019-12-15 21:57:50 +0800

    用户管理模块接口

--- src/api/user.js
+++ src/api/user.js
@@ -1,24 +1,35 @@
+/*
+ * @Descripttion: 用户管理
+ * @version: 1.0
+ * @Author: JDD
+ * @Date: 2019-12-14 20:00:44
+ * @LastEditors: JDD
+ * @LastEditTime: 2019-12-15 20:45:11
+ */
 import request from '@/utils/request'
 
-export function login(data) {
+// 用户列表查询
+export function userList(params) {
   return request({
-    url: '/user/login',
+    url: '/user/list',
+    method: 'get',
+    params
+  })
+}
+
+// 用户新增/修改
+export function userAddOrEdit(url, data) {
+  return request({
+    url,
     method: 'post',
     data
   })
 }
 
-export function getInfo(token) {
+// 用户删除
+export function userDel(params) {
   return request({
-    url: '/user/info',
-    method: 'get',
-    params: { token }
-  })
-}
-
-export function logout() {
-  return request({
-    url: '/user/logout',
-    method: 'post'
+    url: '/user/edit/' + params.id,
+    method: 'get'
   })
 }
