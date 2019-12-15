03cd56688e6031a88b44c9dad8b45768fa37ebc2
Jiangdedong <1308242265@qq.com>
2019-12-15 22:10:11 +0800

    添加路由缓存，权限

--- src/store/getters.js
+++ src/store/getters.js
@@ -4,12 +4,9 @@ const getters = {
   token: state => state.user.token,
   avatar: state => state.user.avatar,
   name: state => state.user.name,
-  roles: state => state.user.roles,
-
   visitedViews: state => state.tagsView.visitedViews,
   cachedViews: state => state.tagsView.cachedViews,
-
-  permission_routers: state => state.permission.routers,
-  addRouters: state => state.permission.addRouters
+  roles: state => state.user.roles,
+  permission_routes: state => state.permission.routes
 }
 export default getters
