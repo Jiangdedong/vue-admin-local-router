9e3b7fc43b7d1fb6c5e824ea7adbbc6c96f64d15
Jiangdedong <1308242265@qq.com>
2019-12-17 21:54:39 +0800

    菜单接口

---
+++ src/api/menu.js
@@ -0,0 +1,35 @@
+/*
+ * @Descripttion: 菜单管理
+ * @version: 1.0
+ * @Author: JDD
+ * @Date: 2019-12-14 20:00:44
+ * @LastEditors: JDD
+ * @LastEditTime: 2019-12-17 21:32:19
+ */
+import request from '@/utils/request'
+
+// 菜单查询
+export function menuList(params) {
+  return request({
+    url: '/menu/list',
+    method: 'get',
+    params
+  })
+}
+
+// 菜单新增/修改
+export function menuAddOrEdit(url, data) {
+  return request({
+    url,
+    method: 'post',
+    data
+  })
+}
+
+// 菜单删除
+export function menuDel(params) {
+  return request({
+    url: '/user/del/' + params.id,
+    method: 'get'
+  })
+}
